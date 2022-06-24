70b61b6dd58e1f4f10e7688fdbd2101e767f43cf
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-06-24 15:53:37 -0500

    Implement disfunctional undo

--- wordle-solver.js
+++ wordle-solver.js
@@ -1,20 +1,25 @@
 import masterWordList from './len5words.js';
+let workingWordList = []; // <-- calculated every time to save bandwidth (~200k every page load) at the cost of CPU cycles
 const cloneMasterWordList = () => masterWordList.map((x) => makeWord(x));
 
 // These character weights are calculated based on the available word list
 const charWeights = {"a": 0.11239067055393585, "b": 0.02681304664723032, "c": 0.032990160349854226, "d": 0.0354682944606414, "e": 0.09938046647230321, "f": 0.014704810495626823, "g": 0.02362427113702624, "h": 0.028990524781341107, "i": 0.06608053935860059, "j": 0.005739795918367347, "k": 0.02243075801749271, "l": 0.05564868804664723, "m": 0.03147776967930029, "n": 0.05455539358600583, "o": 0.06528790087463557, "p": 0.026339285714285714, "q": 0.0014941690962099125, "r": 0.06475036443148688, "s": 0.07543731778425657, "t": 0.04905247813411079, "u": 0.03990524781341108, "v": 0.011361151603498543, "w": 0.013529518950437318, "x": 0.004181851311953352, "y": 0.03144132653061225, "z": 0.006924198250728863};
 
-// define globals
+// Global Constants (so I can change them later if I want)
 const G_WRONG_SPOT = "yellow";
 const G_KNOWN_SPOT = "green";
 const G_INVALID = "red";
-let workingWordList = [];
-let invalidLetters = [];
-let wrongSpot = [[],[],[],[],[]];
-let knownSpot = [null, null, null, null, null];
+
+// Leaving this in for now (allows me to use the developer console to get a list of invalid wordle words)
 let rejectedWords = [];
+
+// For Undo Implementation
 let previousWords = [];
 let currentWord = null;
+
+// Make element lookup and handling cleaner
+// I prefer having a single object that stores DOM elements instead of 
+// a bunch of random global variables to keep track of.
 let elements = {
     add(name, selector) {
         this[name] = document.querySelector(selector);
@@ -27,6 +32,14 @@ let elements = {
         this[element].addEventListener('click', method);
     },
 }
+
+// define globals that determine word search criteria
+// represented by gray letters
+let invalidLetters = [];
+// represented by yellow letters
+let wrongSpot = [[],[],[],[],[]];
+// represented by green letters
+let knownSpot = [null, null, null, null, null];
 
 function initialize() {
     // define element access variables
@@ -58,22 +71,71 @@ function getNextWord() {
 }
 
 function undoCurrentWord(e) {
-    console.log(e);
-};
+    // Roll the display back
+    const word = previousWords.pop();
+    currentWord.wordElement.remove();
+    currentWord = word;
 
-function snapshotCurrentWord() {
-    // currentWord.
-}
+    // Roll baked data back
+    invalidLetters = currentWord.invalidLetters;
+    currentWord.invalidLetters = null;
+    wrongSpot = currentWord.wrongSpot;
+    currentWord.wrongSpot = null;
+    knownSpot = currentWord.knownSpot;
+    currentWord.knownSpot = null;
+
+    // Enable Button Clicks
+    currentWord.enableAllButtons();
+};
 
 function bakeCurrentWord() {
     if(currentWord === null) return;
-    currentWord.updateInvalidLetters(invalidLetters);
-    currentWord.updateWrongSpot(wrongSpot);
-    currentWord.updateKnownSpot(knownSpot);
+    bakeInvalidLetters(invalidLetters);
+    bakeWrongSpot(wrongSpot);
+    bakeKnownSpot(knownSpot);
     currentWord.disableAllButtons();
 
     // push the buttons into an array in case I need to undo
     previousWords.push(currentWord);
+}
+
+
+function bakeInvalidLetters(invalidLetterArray) {
+    // Clone invalidLetters and store into currentWord.invalidLetters
+    const backupInvalidLettersString = JSON.stringify(invalidLetters);
+    currentWord.invalidLetters = JSON.parse(backupInvalidLettersString);
+
+    // perform the bake
+    for(let x=0; x<5; x++) if(currentWord.isInvalidLetter(x)) invalidLetterArray.push(currentWord.word[x]);
+}
+
+function bakeWrongSpot(wrongSpotReference) {
+    // Clone wrongSpot and store into currentWord.wrongSpot
+    const backupWrongSpotString = JSON.stringify(wrongSpot);
+    currentWord.wrongSpot = JSON.parse(backupWrongSpotString);
+
+    // perform the bake
+    for(let x=0; x<wrongSpotReference.length; x++) {
+        const character = currentWord.word[x];
+        // user already tried this letter at this position(...)
+        if(wrongSpotReference[x].includes(character)) continue;
+        // push the character into the array, if it has the G_WRONG_SPOT class applied to the button
+        if(currentWord.isWrongSpot(x)) wrongSpotReference[x].push(character);
+    }
+}
+
+function bakeKnownSpot(knownSpotReference) {
+    // Clone knownSpot and store into currentWord.knownSpot
+    const backupKnownSpotString = JSON.stringify(knownSpot);
+    currentWord.knownSpot = JSON.parse(backupKnownSpotString);
+
+    // perform the bake
+    for(let x=0; x<knownSpotReference.length; x++) {
+        // continue to the next character if I already know the letter in this position...
+        if(knownSpotReference[x] !== null) continue;
+        // assign the letter, if the user has set it to 'known'
+        if(currentWord.isKnownSpot(x)) knownSpot[x] = word[x];
+    }
 }
 
 function findBestNextWord() {
@@ -99,6 +161,7 @@ function findBestNextWord() {
 function makeUIWord(word) {
     return {
         word,
+        wordElement: null,
         0: null,
         1: null,
         2: null,
@@ -155,32 +218,6 @@ function makeUIWord(word) {
                 this.addEventListener(x);
             }
         },
-
-        updateInvalidLetters(invalidLetterArray) {
-            for(let x=0; x<5; x++) {
-                if(this.isInvalidLetter(x)) invalidLetterArray.push(this.word[x]);
-            }
-        },
-
-        updateWrongSpot(wrongSpotReference) {
-            for(let x=0; x<wrongSpotReference.length; x++) {
-                const character = this.word[x];
-                // user already tried this letter at this position(...)
-                if(wrongSpotReference[x].includes(character)) continue;
-                // push the character into the array, if it has the G_WRONG_SPOT class applied to the button
-                if(this.isWrongSpot(x)) wrongSpotReference[x].push(character);
-            }
-        },
-
-        updateKnownSpot(knownSpotReference) {
-            for(let x=0; x<knownSpotReference.length; x++) {
-                // continue to the next character if I already know the letter in this position...
-                if(knownSpotReference[x] !== null) continue;
-                // assign the letter, if the user has set it to 'known'
-                if(this.isKnownSpot(x)) knownSpot[x] = word[x];
-            }
-        }
-
     }
 }
 
@@ -195,8 +232,8 @@ function createWordElement(word) {
     let wordArray = word.toLowerCase().split('');
     wordElement.classList.add('word');
 
+    // Create each letter button
     for(let x=0; x<wordArray.length; x++) {
-        // Create each letter button
         let characterButton = document.createElement('button');
         characterButton.classList.add('square');
         characterButton.textContent = wordArray[x];
@@ -220,8 +257,6 @@ function createWordElement(word) {
     return wordElement;
 }
 
-
-
 function getWordValue(word) {
     let sum = 0;
     let unique = [...new Set(word.toLowerCase())];
@@ -232,7 +267,6 @@ function getWordValue(word) {
     return sum;
 }
 
-// An object that functions as a C# Tuple.. ish
 function makeWord(word) {
     return {
         word: word.toLowerCase(),
@@ -241,16 +275,12 @@ function makeWord(word) {
 }
 
 function hasInvalidLetters(word) {
-    for(const c of invalidLetters) {
-        if(word.includes((c))) return true;
-    }
+    for(const c of invalidLetters) if(word.includes((c))) return true;
     return false;
 }
 
 function hasWrongSpotLetters(word) {
-    for(let x=0; x<wrongSpot.length; x++) {
-        if(wrongSpot[x].includes(word[x])) return true;
-    }
+    for(let x=0; x<wrongSpot.length; x++) if(wrongSpot[x].includes(word[x])) return true;
     return false;
 }
 
