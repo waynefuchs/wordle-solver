ff9e6b59ce918ab0778d08b483ccd289dcdbc85d
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-06-24 14:30:39 -0500

    Baseline program functioning

--- wordle-solver.js
+++ wordle-solver.js
@@ -1,14 +1,19 @@
 import masterWordList from './len5words.js';
-const cloneMasterWordList = () => masterWordList.map((x) => x);
+const cloneMasterWordList = () => masterWordList.map((x) => makeWord(x));
 
 // These character weights are calculated based on the available word list
 const charWeights = {"a": 0.11239067055393585, "b": 0.02681304664723032, "c": 0.032990160349854226, "d": 0.0354682944606414, "e": 0.09938046647230321, "f": 0.014704810495626823, "g": 0.02362427113702624, "h": 0.028990524781341107, "i": 0.06608053935860059, "j": 0.005739795918367347, "k": 0.02243075801749271, "l": 0.05564868804664723, "m": 0.03147776967930029, "n": 0.05455539358600583, "o": 0.06528790087463557, "p": 0.026339285714285714, "q": 0.0014941690962099125, "r": 0.06475036443148688, "s": 0.07543731778425657, "t": 0.04905247813411079, "u": 0.03990524781341108, "v": 0.011361151603498543, "w": 0.013529518950437318, "x": 0.004181851311953352, "y": 0.03144132653061225, "z": 0.006924198250728863};
 
 // define globals
-let workingWordList = [];   // masterWordList - rejected words
-let wrongSpot = [];         // array of arrays containing letters in the wrong spot
-let correctSpot = [];       // array containing correctly identified letters
-// let badWordArray = [];      // for debugging (and so i can remove the words)
+const G_WRONG_SPOT = "yellow";
+const G_KNOWN_SPOT = "green";
+let workingWordList = [];
+let greyLetters = [];
+let wrongSpot = [[],[],[],[],[]];
+let knownSpot = [null, null, null, null, null];
+let rejectedWords = [];
+let previousWords = [];
+let currentWord = null;
 let elements = {
     add(name, selector) {
         this[name] = document.querySelector(selector);
@@ -16,49 +21,183 @@ let elements = {
             console.error("Failed to add " + name + ":" + selector);
             delete this[name];
         }
-    }
+    },
+    addEventListener(element, method) {
+        this[element].addEventListener('click', method);
+        this[element].addEventListener('touchstart', method);
+    },
 }
 
 function initialize() {
+    // define element access variables
     elements.add("output", "#output");
     elements.add("words", "#word-array");
+    elements.add("next-move", "#find-next-move");
+    elements.add("not-accepted", "#word-not-accepted");
+
+    // load and sort the word list
     workingWordList = cloneMasterWordList();
+    workingWordList.sort((a, b) => a.value > b.value ? -1 : a.value < b.value ? 1 : 0);
+    
+    // wire events
+    elements.addEventListener("next-move", getNextWord);
+    elements.addEventListener("not-accepted", wordNotAccepted);
+
+    
+    getNextWord();
+    console.log("Initialized.");
 } initialize();
 
+function getNextWord() {
+    bakeCurrentWord();
+    var suggestion = findBestNextWord();
+    setOutput(suggestion.word);
+    let newWord = createWordElement(suggestion.word);
+    elements.words.append(newWord);
+}
 
-function createWord(word) {
+function bakeCurrentWord() {
+    if(currentWord === null) return;
+    currentWord.updateGreyLetters(greyLetters);
+    currentWord.updateWrongSpot(wrongSpot);
+    currentWord.updateKnownSpot(knownSpot);
+    currentWord.disableAllButtons();
+
+    // push the buttons into an array in case I need to undo
+    previousWords.push(currentWord);
+}
+
+function findBestNextWord() {
+    for(const wordObj of workingWordList) {
+        const word = wordObj.word;
+        // Go to the next word in the list if:
+        // 1. Word contains any letters that are known to be wrong
+        if(hasGreyLetters(word)) continue;
+
+        // 2. Word contains any "wrong spot" letters in the known "wrong spot"
+        if(hasWrongSpotLetters(word)) continue;
+
+        // 3. Word contains an incorrect letter in a "known spot"
+        // 4. Word must match "known spot" letters (This is implied)
+        if(hasIncorrectLetterInKnownSpot(word)) continue;
+
+        return wordObj;
+    }
+    return makeWord("#####");
+}
+
+// An object that stores details about the buttons in each word
+function makeUIWord(word) {
+    return {
+        word,
+        0: null,
+        1: null,
+        2: null,
+        3: null,
+        4: null,
+        isInvalidLetter(index) {
+            if(this[index] === null) return false;
+            if(this[index].classList.contains(G_WRONG_SPOT) 
+                || this[index].classList.contains(G_KNOWN_SPOT)) return false;
+            return true;
+        },
+        isWrongSpot(index) {
+            if(this[index] === null) return false;
+            return this[index].classList.contains(G_WRONG_SPOT);
+        },
+        isKnownSpot(index) {
+            if(this[index] === null) return false;
+            return this[index].classList.contains(G_KNOWN_SPOT);
+        },
+        getIndex(button) {
+            for(let x=0; x<5; x++) {
+                if(button === this[x]) return x;
+            }
+            console.error("Button not found in object!");
+            return -1;
+        },
+        letterButtonPushed(e) {
+            const c = this.classList;
+            if(c.contains(G_WRONG_SPOT)) {
+                c.remove(G_WRONG_SPOT);
+                c.add(G_KNOWN_SPOT);
+            } else if(c.contains(G_KNOWN_SPOT)) {
+                c.remove(G_KNOWN_SPOT);
+            } else {
+                c.add(G_WRONG_SPOT);
+            }
+        },
+        addEventListener(index) {
+            this[index].addEventListener('click', this.letterButtonPushed);
+            this[index].addEventListener('touchstart', this.letterButtonPushed);
+        },
+        removeEventListener(index) {
+            this[index].removeEventListener('click', this.letterButtonPushed);
+            this[index].removeEventListener('touchstart', this.letterButtonPushed);
+        },
+        disableAllButtons() {
+            for(let x=0; x<5; x++) {
+                this.removeEventListener(x);
+            }
+        },
+
+        updateGreyLetters(grayLetterArray) {
+            for(let x=0; x<5; x++) {
+                if(this.isInvalidLetter(x)) grayLetterArray.push(this.word[x]);
+            }
+        },
+
+        updateWrongSpot(wrongSpotReference) {
+            for(let x=0; x<wrongSpotReference.length; x++) {
+                const character = this.word[x];
+                // user already tried this letter at this position(...)
+                if(wrongSpotReference[x].includes(character)) continue;
+                // push the character into the array, if it has the G_WRONG_SPOT class applied to the button
+                if(this.isWrongSpot(x)) wrongSpotReference[x].push(character);
+            }
+        },
+
+        updateKnownSpot(knownSpotReference) {
+            for(let x=0; x<knownSpotReference.length; x++) {
+                // continue to the next character if I already know the letter in this position...
+                if(knownSpotReference[x] !== null) continue;
+                // assign the letter, if the user has set it to 'known'
+                if(this.isKnownSpot(x)) knownSpot[x] = word[x];
+            }
+        }
+
+    }
+}
+
+function createWordElement(word) {
+    // Push current word onto undo stack, if applicable
+    bakeCurrentWord();
+    // get a new UI object
+    currentWord = makeUIWord(word);
+
+    // Create the containing div
     let wordElement = document.createElement('div');
     let wordArray = word.toLowerCase().split('');
     wordElement.classList.add('word');
+
     for(let x=0; x<wordArray.length; x++) {
+        // Create each letter button
         let characterButton = document.createElement('button');
         characterButton.classList.add('square');
-        characterButton.value = wordArray[x];
         characterButton.textContent = wordArray[x];
-        createCharacterEventListener(characterButton);
+
+        // Add the button to the currentWord UI object
+        currentWord[x] = characterButton;
+        currentWord.addEventListener(x);
+
+        // Add an element reference
+        currentWord.wordElement = wordElement;
+
+        // Add the button to the containing div
         wordElement.append(characterButton);
     }
     return wordElement;
 }
-
-function characterPushed(e) {
-    console.log(e);
-    const c = e.target.classList;
-    if(c.contains('yellow')) {
-        c.remove('yellow');
-        c.add('green');
-    } else if(c.contains('green')) {
-        c.remove('green');
-    } else {
-        c.add('yellow');
-    }
-}
-
-function createCharacterEventListener(button) {
-    button.addEventListener('click', characterPushed);
-    button.addEventListener('touchstart', characterPushed);
-}
-
 
 
 
@@ -72,93 +211,63 @@ function getWordValue(word) {
     return sum;
 }
 
+// An object that functions as a C# Tuple.. ish
 function makeWord(word) {
-    let wordlc = word.toLowerCase();
-    let value = getWordValue(word);
     return {
-        word,
-        wordlc,
-        value,
+        word: word.toLowerCase(),
+        value: getWordValue(word),
     };
 }
 
-function isMatchingKnownLetters(testWord) {
-    return (testWord[0] === c0.value || c0.value === "")
-        && (testWord[1] === c1.value || c1.value === "")
-        && (testWord[2] === c2.value || c2.value === "")
-        && (testWord[3] === c3.value || c3.value === "")
-        && (testWord[4] === c4.value || c4.value === "");
-}
-
-function isNotContainingBadLetters(testWord) {
-    for(const c of wrongSpot) {
-        if(testWord.includes(c)) return false;
+function hasGreyLetters(word) {
+    for(const c of greyLetters) {
+        if(word.includes((c))) return true;
     }
-    return true;
+    return false;
 }
 
-function isMatchingGoodLetters(testWord) {
-    for(const c of correctSpot) {
-        if(!testWord.includes(c)) return false;
+function hasWrongSpotLetters(word) {
+    for(let x=0; x<wrongSpot.length; x++) {
+        if(wrongSpot[x].includes(word[x])) return true;
     }
-    return true;
+    return false;
 }
 
-function isNotMatchingBadPositionLetters(testWord) {
-    for(let x=0; x<outOfPositionElements.length; x++) {
-        if(outOfPositionElements[x].value === "") continue;
-        for(const c of outOfPositionElements[x].value.toLowerCase()) {
-            if(testWord[x] === c) return false;
+function hasIncorrectLetterInKnownSpot(word) {
+    for(let x=0; x<knownSpot.length; x++) {
+        if(knownSpot[x] === null) continue;
+        if(knownSpot[x] !== word[x]) return true;
+    }
+    return false;
+}
+
+function removeWordFromWorkingWordList(word) {
+    for(let x=0; x<workingWordList.length; x++) {
+        if(workingWordList[x].word === word) {
+            const removed = workingWordList.splice(x, 1);
+            return;
         }
     }
-    return true;
 }
 
-function getBestWord(list) {
-    var bestObj = makeWord("");
-    for(const obj of list) {
-        if(isMatchingKnownLetters(obj.wordlc) 
-        && isNotContainingBadLetters(obj.wordlc)
-        && isMatchingGoodLetters(obj.wordlc)
-        && isNotMatchingBadPositionLetters(obj.wordlc)
-        && obj.value > bestObj.value) {
-            bestObj = obj;
-        }
-    }
-    if(bestObj.word === "") bestObj.word = "No valid word was found!";
-    return bestObj;
-}
+function wordNotAccepted(e) {
+    console.log(e);
 
-function getBadLettersFromUser() {
-    wrongSpot = [];
-    const letters = badLetterElement.value;
-    for(const c of letters.toLowerCase()) {
-        wrongSpot.push(c);
-    }
+    const word = currentWord.word;
+    // reject the word and remove it from the working list
+    rejectedWords.push(word);
+    removeWordFromWorkingWordList(word);
+
+    // remove the element containing the buttons from the page
+    currentWord.wordElement.remove();
+    currentWord = null;
+
+    // generate a new word
+    getNextWord();
 }
 
 function setOutput(message) {
     elements.output.textContent = message;
 }
 
-function suggestWord() {
-
-}
-
-function recalculateNextMove() {
-    getBadLettersFromUser();
-    workingWordList = loadWords();
-    
-    
-    outOfPositionArray = getBadPosition();
-    let bestWord = getBestWord(workingWordList);
-    setOutput(bestWord.word)
-
-    let newWord = createWord(bestWord.word);
-    elementWordArray.append(newWord.wordElement);
-}
-
-function removeWord(word) {
-    badWordArray.push(word);
-}
 
